cef0d02b481d20891e49e7b710509826990af872
titomon98 <arturomonterroso3@gmail.com>
2025-06-25 13:47:53 -0600

    Iniciando microservicio de items

---
+++ src/main/java/com/msvc/items/items_microservice/ItemsMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.msvc.items.items_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemsMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemsMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/msvc/items/items_microservice/ItemsMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msvc.items.items_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemsMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
